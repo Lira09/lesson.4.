2c3ed71979e1975842ee43d659ba6c6f5fafedb6
Lira09 <nazirbekova.lira@gmail.com>
2021-03-03 19:01:07 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,123 @@
+package com.company;
+import java.util.Random;
+public class Main {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefenceType = "";
+    public static int[] heroesHealth = {260, 270, 250 , 220};
+    public static int[] heroesDamage = {20, 15, 10 , 0 };
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic" ,"Medic"};
+    public static int roundNumber = 0;
+
+    public static void main(String[] args) {
+        printStatistics();
+        while (!isGameFinished()) {
+            round();
+        }
+    }
+
+    public static void round() {
+        roundNumber++;
+        System.out.println("ROUND №" + roundNumber);
+        changeDefence();
+        if (bossHealth > 0) {
+            bossHits();
+        }
+        medicHeal();
+        heroesHit();
+        printStatistics();
+
+    }
+
+    public static void changeDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length);
+        bossDefenceType = heroesAttackType[randomIndex];
+        System.out.println("Boss chose: " + bossDefenceType);
+    }
+
+    public static boolean isGameFinished() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+        }
+        return allHeroesDead;
+    }
+
+
+
+    public static void printStatistics() {
+        System.out.println("_______________________");
+        System.out.println("Boss health: " + bossHealth + " [" + bossDamage + "]");
+        for (int i = 0; i < heroesAttackType.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: "
+                    + heroesHealth[i] + " [" + heroesDamage[i] + "]");
+        }
+    }
+
+
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+            }
+        }
+    }
+
+    public static void heroesHit() {
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (bossHealth > 0 && heroesHealth[i] > 0) {
+                if (bossDefenceType == heroesAttackType[i]) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(10) + 2;
+                    if (bossHealth - heroesDamage[i] * coeff < 0) {
+                        bossHealth = 0;
+                    } else {
+                        bossHealth = bossHealth - heroesDamage[i] * coeff;
+                    }
+                    System.out.println("Critical damage: " + heroesDamage[i] * coeff);
+                } else {
+                    if (bossHealth - heroesDamage[i] < 0) {
+                        bossHealth = 0;
+                    } else {
+                        bossHealth = bossHealth - heroesDamage[i];
+                    }
+                }
+            }
+        }
+    }
+
+    public static void medicHeal() {
+
+        Random random = new Random();
+        int isHeal = random.nextInt(60);
+        for (int i = 0; i < heroesHealth.length; i++) {
+
+            int hero = (int) Math.floor(Math.random() * heroesAttackType.length);
+            if (heroesHealth[hero] < 100 && heroesHealth[3] > 0) {
+                heroesHealth[hero] =heroesHealth[hero] + isHeal;
+            }
+            System.out.println(isHeal + hero);
+            break;
+
+
+        }
+
+    }
+
+}
