622a2014f993889ed76af57a1520a83b7e631958
Lira09 <nazirbekova.lira@gmail.com>
2021-03-04 16:15:37 +0600

    kvhg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,21 @@
 package com.company;
+
+import java.net.PortUnreachableException;
 import java.util.Random;
+
 public class Main {
-    public static int bossHealth = 700;
+    public static final int GOLEM = 4;
+    public static final int LUCKY = 5;
+    public static final int BERSERK = 6;
+    public static final int THOR = 7;
+    public static int bossHealth = 850;
     public static int bossDamage = 50;
     public static String bossDefenceType = "";
-    public static int[] heroesHealth = {260, 270, 250 , 220};
-    public static int[] heroesDamage = {20, 15, 10 , 0 };
-    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic" ,"Medic"};
+    public static int[] heroesHealth = {260, 270, 250, 220, 350, 110, 200, 210};
+    public static int[] heroesDamage = {20, 15, 10, 0, 10, 15, 20, 15};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic", "Golem", "Lucky", "Berserk", "Thor"};
     public static int roundNumber = 0;
+    public static int randomChance = 0;
 
     public static void main(String[] args) {
         printStatistics();
@@ -17,12 +25,19 @@ public class Main {
     }
 
     public static void round() {
+        Random random = new Random();
+        randomChance = random.nextInt(5) + 1;
         roundNumber++;
         System.out.println("ROUND №" + roundNumber);
+        System.out.println("randomChance= " + randomChance);
         changeDefence();
-        if (bossHealth > 0) {
+        if (bossHealth > 0 && !isThorDeaf()) {
             bossHits();
         }
+
+        berserk();
+        lucky();
+        golem();
         medicHeal();
         heroesHit();
         printStatistics();
@@ -55,7 +70,6 @@ public class Main {
     }
 
 
-
     public static void printStatistics() {
         System.out.println("_______________________");
         System.out.println("Boss health: " + bossHealth + " [" + bossDamage + "]");
@@ -64,7 +78,6 @@ public class Main {
                     + heroesHealth[i] + " [" + heroesDamage[i] + "]");
         }
     }
-
 
 
     public static void bossHits() {
@@ -102,22 +115,69 @@ public class Main {
         }
     }
 
+    {
+
+    }
+
+
     public static void medicHeal() {
-
-        Random random = new Random();
-        int isHeal = random.nextInt(60);
-        for (int i = 0; i < heroesHealth.length; i++) {
-
-            int hero = (int) Math.floor(Math.random() * heroesAttackType.length);
-            if (heroesHealth[hero] < 100 && heroesHealth[3] > 0) {
-                heroesHealth[hero] =heroesHealth[hero] + isHeal;
+        //    boolean healit = false;
+        if (heroesHealth[3] > 0) {
+            for (int i = 0; i < heroesHealth.length; i++) {
+                if (heroesHealth[i] < 100 && heroesHealth[i] > 0) {
+                    //     healit = true;
+                    Random r = new Random();
+                    int a = r.nextInt(60);
+                    heroesHealth[i] += a;
+                    System.out.println(heroesAttackType[3] + " is heal " + heroesAttackType[i] + " +" + a + " hp");
+                    break;
+                }
             }
-            System.out.println(isHeal + hero);
-            break;
+        }
 
+    }
+
+    public static void golem() {
+        if (heroesHealth[GOLEM] > 0) {
+            for (int i = 0; i < heroesHealth.length; i++) {
+                if (heroesHealth[i] > 0) {
+                    int damage = bossDamage / 5;
+                    heroesHealth[i] += damage;
+                    if (heroesHealth[GOLEM] - damage < 0) heroesHealth[GOLEM] = 0;
+                    else heroesHealth[GOLEM] -= damage;
+                }
+
+            }
+        }
+
+    }
+
+    public static void lucky() {
+        if (heroesHealth[LUCKY] > 0 && randomChance == 5) {
+            heroesHealth[LUCKY] += 50;
+            System.out.println(" Lucky is avoid");
 
         }
 
     }
 
+    public static void berserk() {
+        if (heroesHealth[BERSERK] > 0) {
+            int berserk = bossDamage - 25;
+            if (bossHealth - berserk < 0) bossHealth = 0;
+            else bossHealth -= berserk;
+            System.out.println(heroesAttackType[6] + " is blocked!");
+        }
+    }
+
+    public static boolean isThorDeaf() {
+        if (heroesHealth[THOR] > 0 && randomChance == 2) {
+            System.out.println("Thor stunned Boss!");
+            return true;
+        } else return false;
+    }
+
 }
+
+
+
